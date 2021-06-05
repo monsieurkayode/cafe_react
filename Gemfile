6acdfac7e3ef1cf1a1df2d2969c5a68c38fd1b6b
Kayode Adeola <monsieurkayode@gmail.com>
2021-06-05 05:44:57 +0100

    Add unit test for menu resource

--- Gemfile
+++ Gemfile
@@ -41,6 +41,10 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails'
+  gem 'factory_bot'
+  gem 'capybara'
+  gem 'database_cleaner'
   gem 'ffaker'
 end
 
@@ -51,10 +55,6 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-  gem 'rspec-rails', '~> 3.5'
-  gem 'factory_girl_rails'
-  gem 'capybara'
-  gem 'database_cleaner'
 end
 
 
---
+++ spec/model/menu_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+FactoryBot.define do
+  factory :menu do
+    name { }
+    type { }
+    price { }
+    photo_url { }
+  end
+end
+
+RSpec.describe Menu, type: :model do
+  let(:menu) { build(:menu, name: 'Fish', type: 'side', price: 20) }
+  subject { menu }
+
+  it { should respond_to(:type) }
+  it { should respond_to(:name) }
+  it { should respond_to(:price) }
+  it { should respond_to(:photo_url) }
+  it { should be_valid }
+
+  describe "should be invalid if name is blank" do
+    let(:menu) { build(:menu, type: 'side', price: 20)}
+    it { should_not be_valid }
+  end
+
+  describe "should be invalid if type is blank" do
+    let(:menu) { build(:menu, name: 'Crayfish', price: 20)}
+    it { should_not be_valid }
+  end
+
+  describe "should be invalid if price is blank" do
+    let(:menu) { build(:menu, name: 'Crayfish')}
+    it { should_not be_valid }
+  end
+
+  describe "should be valid if no photo is uploaded" do
+    let(:menu) { build(:menu, type: 'soup', price: 20, name: 'Crayfish')}
+    it { should be_valid }
+  end
+end
