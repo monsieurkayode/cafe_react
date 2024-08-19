d6a50d09d5594bd6d0c3bbfc4528395e6797b91c
Kayode Adeola <monsieurkayode@gmail.com>
2024-08-19 21:41:27 +0100

    Load factory bot syntax in rspec

---
+++ spec/support/factory_bot.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.include FactoryBot::Syntax::Methods
+end
