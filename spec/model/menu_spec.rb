60bec506399706329c9924f9a25e6388bf5bae82
Kayode Adeola <monsieurkayode@gmail.com>
2024-08-19 21:51:29 +0100

    Rubocop

--- spec/model/menu_spec.rb
+++ spec/model/menu_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Menu do
-  subject { menu }
+  subject(:menu) { menu }
 
   describe '#associations' do
     let(:menu) { build(:menu, name: 'Fish', type: 'side', price: 20) }
@@ -14,23 +14,25 @@ RSpec.describe Menu do
   end
 
   describe '#validations' do
-    context 'should be invalid if name is blank' do
+    context 'when name is blank' do
       let(:menu) { build(:menu, name: nil) }
 
       it { is_expected.not_to be_valid }
+
       it 'raises and ActiveRecord::RecordInvalid error' do
-        expect { subject.save! }.to raise_error(
+        expect { menu.save! }.to raise_error(
           ActiveRecord::RecordInvalid
         ).with_message('Validation failed: Name can\'t be blank')
       end
     end
 
-    context 'should be invalid if type is blank' do
+    context 'when type is blank' do
       let(:menu) { build(:menu, type: nil) }
 
       it { is_expected.not_to be_valid }
+
       it 'raises and ActiveRecord::RecordInvalid error' do
-        expect { subject.save! }.to raise_error(
+        expect { menu.save! }.to raise_error(
           ActiveRecord::RecordInvalid
         ).with_message('Validation failed: Type can\'t be blank')
       end
@@ -40,8 +42,9 @@ RSpec.describe Menu do
       let(:menu) { build(:menu, price: nil) }
 
       it { is_expected.not_to be_valid }
+
       it 'raises and ActiveRecord::RecordInvalid error' do
-        expect { subject.save! }.to raise_error(
+        expect { menu.save! }.to raise_error(
           ActiveRecord::RecordInvalid
         ).with_message('Validation failed: Price can\'t be blank')
       end
@@ -51,8 +54,9 @@ RSpec.describe Menu do
       let(:menu) { build(:menu, photo_url: nil) }
 
       it { is_expected.to be_valid }
+
       it 'raises and ActiveRecord::RecordInvalid error' do
-        expect { subject.save! }.not_to raise_error
+        expect { menu.save! }.not_to raise_error
       end
     end
   end
